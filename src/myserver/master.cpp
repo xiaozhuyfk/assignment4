e18517970d0484caf022ee7b777350201a49f8bc
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-04-03 17:07:42 -0400

    hongyul

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -496,21 +496,29 @@ void distribute_job(Request_msg& req) {
     } else if (req.get_arg("cmd") == "projectidea") {
         DLOG(INFO) << "projectidea start" << std::endl;
         if (mstate.idle_workers.size() == 0) {
+            DLOG(INFO) << 1 << std::endl;
             if (mstate.worker_roster.size() + mstate.requested_workers <
                     mstate.max_num_workers) {
                 request_new_worker();
             }
+            DLOG(INFO) << 2 << std::endl;
             mstate.pending_cached_jobs.push(tag);
+            DLOG(INFO) << 3 << std::endl;
         } else {
+            DLOG(INFO) << 4 << std::endl;
             Worker_handle job_receiver = mstate.idle_workers.front();
+            DLOG(INFO) << 5 << std::endl;
             mstate.idle_workers.pop();
             mstate.worker_roster[job_receiver].processing_cached_job = true;
             mstate.worker_roster[job_receiver].job_count++;
             mstate.worker_roster[job_receiver].idle_time = 0;
             mstate.worker_roster[job_receiver].work_estimate[1] +=
                     work_estimate(req);
+            DLOG(INFO) << 6 << std::endl;
             req.set_thread_id(1);
+            DLOG(INFO) << 7 << std::endl;
             send_request_to_worker(job_receiver, req);
+            DLOG(INFO) << 8 << std::endl;
         }
         DLOG(INFO) << "distribute end projectidea" << std::endl;
     }
