fef5825b069186917cbd8db9bad9267c3dbfed1c
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-04-05 02:05:11 -0400

    aff

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -405,9 +405,19 @@ void handle_tick() {
     }
     */
 
-    if (mstate.worker_roster.size() + mstate.requested_workers <
+    if (mstate.worker_roster.size() + mstate.requested_workers + 1 <
+                mstate.max_num_workers) {
+        if (mstate.pending_requests.size() > 12 ||
+                mstate.pending_cached_jobs.size() > 1) {
+            request_new_worker();
+            request_new_worker();
+        } else if (mstate.pending_requests.size() > 0 ||
+                mstate.pending_cached_jobs.size() > 0) {
+            request_new_worker();
+        }
+    } else if (mstate.worker_roster.size() + mstate.requested_workers <
             mstate.max_num_workers) {
-        if (mstate.pending_requests.size() > 20 ||
+        if (mstate.pending_requests.size() > 0 ||
                 mstate.pending_cached_jobs.size() > 0) {
             request_new_worker();
         }
