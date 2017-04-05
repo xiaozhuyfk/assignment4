88c28c46963cde6c158c026118a48ba51873c3f9
xiaozhuyfk <xiaozhuyfk@gmail.com>
2017-04-05 02:01:26 -0400

    aff

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -408,7 +408,7 @@ void handle_tick() {
     if (mstate.worker_roster.size() + mstate.requested_workers <
             mstate.max_num_workers) {
         if (mstate.pending_requests.size() > 20 ||
-                mstate.pending_cached_jobs.size() > 2) {
+                mstate.pending_cached_jobs.size() > 0) {
             request_new_worker();
         }
     }
